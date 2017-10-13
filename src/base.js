ea399cc3f90c0457bd37bd12abdc461ce3b531f1
Michael <5453237+mwhittet@users.noreply.github.com>
2017-10-13 15:39:31 +0100

    Setting up firebase.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCKhI5YszUXinzaMI28shB2VegA0T7KZI0",
+  authDomain: "catch-of-the-day-9c25b.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-9c25b.firebaseio.com",
+});
+
+export default base;
