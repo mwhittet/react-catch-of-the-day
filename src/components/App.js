78319e743d5399d33252b8a6ca71bf97429fa8d8
Michael <5453237+mwhittet@users.noreply.github.com>
2017-10-13 16:55:57 +0100

    Bi directional flow and live state editing.

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     super();
     
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     
@@ -58,6 +59,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
   
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+  
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -87,11 +94,16 @@ class App extends React.Component {
           </ul>
         </div>
         <Order 
-          fishes={this.state.fishes} 
-          order={this.state.order} 
+          fishes={this.state.fishes}
+          order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory 
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
