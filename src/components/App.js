0682a7e9c8adc8c653dbc237b77661430d01196a
Michael <5453237+mwhittet@users.noreply.github.com>
2017-10-13 16:01:50 +0100

    Use local storage to store order details.

--- src/components/App.js
+++ src/components/App.js
@@ -22,14 +22,30 @@ class App extends React.Component {
   }
   
   componentWillMount() {
+    // This runs right before the <App> is rendered.
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
-  }
+    
+    // Check local storage for orders.
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    
+    if(localStorageRef) {
+      // Update our <App> component's order state.
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  } 
   
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  }
+  
+  componentWillUpdate(nextProps, nextState) {
+    //console.log({nextProps, nextState});
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
   
   addFish(fish) {
@@ -70,7 +86,11 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order} 
+          params={this.props.params}
+        />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
